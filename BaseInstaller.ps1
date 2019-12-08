e22f63ef85153b115afdbfbac08facb10548fbc2
manicman <manicman@users.noreply.github.com>
2019-12-08 08:59:52 -0500

    Initial Checkin

---
+++ BaseInstaller.ps1
@@ -0,0 +1,15 @@
+# install/Boostrap Chocolatey. Detailed instructions: https://chocolatey.org/install
+# Requires -RunAsAdministrator, Set-ExecutionPolicy Bypass -Scope Process
+$Chocoinstalled = $false
+if (get-command choco.exe -ErrorAction SilentlyContinue){
+    $Chocoinstalled = $true
+}
+
+if (!$Chocoinstalled) {
+    Write-Output "About to install Chocolately"
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+#Base installs from community repo: https://chocolatey.org/packages
+Get-Content ".\packagelist" | ForEach-Object {$_ -split "\r\n"} | ForEach-Object {choco install -y $_}
