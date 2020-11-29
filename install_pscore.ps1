7bf6845cd6a0383a661b541ab8d9f4d870b4a753
Chris Pollock <73022650+ChristopherPollock@users.noreply.github.com>
2020-11-29 10:07:52 -0500

    Added console notifications for events.

--- install_pscore.ps1
+++ install_pscore.ps1
@@ -1,6 +1,7 @@
 # installs Powershell Core and sets up environment.  Once this is added to the community choco repo will deprecate this script.
-$PSInstaller="PowerShell-7.1.0-win-x64.msi"
 write-output "Downloading Powershell Core installer..."
+
+$PSInstaller="PowerShell-7.1.0-win-x64.msi"
 Invoke-WebRequest https://github.com/PowerShell/PowerShell/releases/download/v7.1.0/PowerShell-7.1.0-win-x64.msi -outfile  $PSInstaller
 write-output "Installing Powershell Core..."
 Start-Process .\$PSInstaller  -Wait -ArgumentList '/quiet'
@@ -8,6 +9,7 @@ remove-item .\$PSInstaller
 
 # If using PowerShell Core, install PSReadline:
 write-output "Installing PSReadline in support of ""Powerline"" customization for Windows Terminal"
+
 Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
 
 write-output "Completed Powershell Core installation!"
--- install_wsl2.ps1
+++ install_wsl2.ps1
@@ -1,6 +1,9 @@
 # Downloads and Installs the required update for WSL2
 write-output "Installing the WSL2 update from https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
+
 $WSLUpdateFile="wsl_update_x64.msi"
 Invoke-WebRequest https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -outfile $WSLUpdateFile
 Start-Process .\$WSLUpdateFile  -Wait -ArgumentList '/quiet'
 remove-item .\$WSLUpdateFile
+
+write-output "Completed the WSL2 update!"
