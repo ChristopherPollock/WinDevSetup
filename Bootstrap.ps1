2e80a0300fc448c38703edde41fca62f275d05c2
Chris Pollock <73022650+ChristopherPollock@users.noreply.github.com>
2024-01-27 22:01:18 -0500

    refactored app installer logic and added a few packages tto the packagelist

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -46,6 +46,41 @@ wsl --install
 wsl --install -d kali-linux
 wsl --install -d Ubuntu
 
+#Install WinGet
+#Based on this gist: https://gist.github.com/crutkas/6c2096eae387e544bd05cde246f23901
+$hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
+if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
+    "Installing winget Dependencies"
+    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+
+    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $releases = Invoke-RestMethod -uri $releases_url
+    $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
+
+    "Installing winget from $($latestRelease.browser_download_url)"
+    Add-AppxPackage -Path $latestRelease.browser_download_url
+}
+else {
+    "winget already installed"
+}
+#Configure WinGet
+Write-Output "Configuring winget"
+
+#winget config path from: https://github.com/microsoft/winget-cli/blob/master/doc/Settings.md#file-location
+$settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json";
+$settingsJson = 
+@"
+    {
+        // For documentation on these settings, see: https://aka.ms/winget-settings
+        "experimentalFeatures": {
+          "experimentalMSStore": true,
+        }
+    }
+"@;
+$settingsJson | Out-File $settingsPath -Encoding utf8
+
 # install/Boostrap Chocolatey. Detailed instructions: https://chocolatey.org/install
 $Chocoinstalled = $false
 if (get-command choco.exe -ErrorAction SilentlyContinue){
@@ -57,12 +92,42 @@ if (!$Chocoinstalled) {
     Set-ExecutionPolicy Bypass -Scope Process -Force
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
+
 #Winget installs (try to use winget by default, use choco for packages that aren't available in the Microsoft public repos) https://chocolatey.org/packages
-Get-Content ".\winget_PackageList" | ForEach-Object {$_ -split "\r\n"} | ForEach-Object {
+# Define the path to your text file
+$textFilePath = ".\winget_PackageList"
+
+# Create an empty hash table
+$Applist = @{}
+
+# Initialize an index counter
+$index = 0
+
+# Read each line from the file
+Get-Content $textFilePath | ForEach-Object {
+    # Increment the index counter
+    $index++
+
+    #check if the item is not a comment 
     if ($_.substring(0,1) -ne '#') {
-        winget install $_ --silent --accept-package-agreements --accept-source-agreements
+        # Add the line (as key) and index (as value) to the hash table
+        $Applist[$_.Trim()] = $index
     }
 }
+
+#install the apps
+foreach ($key in $Applist.keys) {
+    $listApp = winget list -q $key
+    #Write-host  $Applist[$key] $key `n $listApp 
+    if (![String]::Join("", $listApp).Contains($key)) {
+        Write-host "Installing:" $key "(line" $Applist[$key] ")"
+        winget install --silent $key --accept-package-agreements
+    }
+    else {
+        Write-host "Skipping Install of" $key "(line" $Applist[$key] ")"
+    }
+}
+
 
 #Choco Installations from community repo: https://chocolatey.org/packages
 Get-Content ".\Choco_PackageList" | ForEach-Object {$_ -split "\r\n"} | ForEach-Object {
@@ -74,10 +139,3 @@ Get-Content ".\Choco_PackageList" | ForEach-Object {$_ -split "\r\n"} | ForEach-
 #install the oh-my-posh fonts  https://ohmyposh.dev/docs/installation/fonts
 $env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
 oh-my-posh font install
-
-# Install  the required update for WSL2
-#.\Install_WSL2.ps1
-
-
-# PowerLine for Windows Terminal install posh-git and oh-my-posh
-#.\Install_Powerline.ps1
